b2a9b74645714e5d4608f389adba82da15515935
Jnourry <jordan.nourry@gmail.com>
2020-03-28 19:08:41 +0100

    First commit

---
+++ src/test/java/io/shodo/kata/sudoku/SudokuGridValidatorTest.java
@@ -0,0 +1,13 @@
+package io.shodo.kata.sudoku;
+
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class SudokuGridValidatorTest {
+
+  @Test
+  void first_test() {
+    assertThat(true).isTrue();
+  }
+}
